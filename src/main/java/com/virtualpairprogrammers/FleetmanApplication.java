7edf2a00fc1b2ae8fc8e78c0813992c7609be050
VPPCourseUser <contact@capabilitytm.com>
2017-02-13 19:35:22 +0000

    Update FleetmanApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
@@ -1,10 +1,16 @@
 package com.virtualpairprogrammers;
 
+
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Value;
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -12,8 +18,21 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableFeignClients
 public class FleetmanApplication {
 
+	@Value("${server.port}")
+        private int port;
+
 	public static void main(String[] args) {
 		SpringApplication.run(FleetmanApplication.class, args);
 	}
 	
+
+       @Bean
+       public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
+       {
+          AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+          instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+          instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+          instance.setDataCenterInfo(info);
+          instance.setNonSecurePort(port);
+       }	
 }
