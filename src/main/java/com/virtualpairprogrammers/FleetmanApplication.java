6b5ee49d517277b569567762d7e0a114b7d299d0
VPPCourseUser <contact@capabilitytm.com>
2017-02-13 19:52:17 +0000

    Update FleetmanApplication.java

--- src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
@@ -6,11 +6,6 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.commons.util.InetUtils;
-import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.beans.factory.annotation.Value;
-import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableCircuitBreaker
@@ -18,23 +13,8 @@ import com.netflix.appinfo.AmazonInfo;
 @EnableFeignClients
 public class FleetmanApplication {
 
-	@Value("${server.port}")
-        private int port;
-
 	public static void main(String[] args) {
 		SpringApplication.run(FleetmanApplication.class, args);
 	}
 	
-
-       @Bean
-       public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
-       {
-          EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);	       
-          AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-          instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-          instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-          instance.setDataCenterInfo(info);
-          instance.setNonSecurePort(port);
-	  return instance;
-       }	
 }
