b5da0718322db265e96527a4b1c2f6fab3019e87
Dick Chesterwood <richard@inceptiontraining.co.uk>
2017-02-13 22:37:51 +0000

    trying transactional

--- src/main/java/com/virtualpairprogrammers/domain/Vehicle.java
+++ src/main/java/com/virtualpairprogrammers/domain/Vehicle.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Version;
 
 @Entity
 public class Vehicle {
@@ -22,6 +23,9 @@ public class Vehicle {
 	private Date lastRecordedPosition;
 	private String currentDriver;
 	private String chassisNumber;
+	
+	@Version
+	private int version;
 	
 	public Vehicle() {}
 	
