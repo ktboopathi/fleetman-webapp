425b6039dc91f300128f5e77a54cda476f366e75
Dick Chesterwood <richard@inceptiontraining.co.uk>
2017-02-14 08:05:22 +0000

    rds removal

--- src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
+++ src/main/java/com/virtualpairprogrammers/FleetmanApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.commons.util.InetUtils;
 import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -18,7 +17,6 @@ import com.netflix.appinfo.AmazonInfo;
 @EnableCircuitBreaker
 @EnableDiscoveryClient
 @EnableFeignClients
-@EnableHystrixDashboard
 @EnableTransactionManagement
 public class FleetmanApplication {
 
--- src/main/java/com/virtualpairprogrammers/services/PositionTrackingExternalService.java
+++ src/main/java/com/virtualpairprogrammers/services/PositionTrackingExternalService.java
@@ -1,8 +1,6 @@
 package com.virtualpairprogrammers.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
